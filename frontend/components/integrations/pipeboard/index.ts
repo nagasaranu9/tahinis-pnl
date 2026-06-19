cc98092ccf3d809c8c370aebd68b9d6e9d1ac51a
Naga Saranu <naga.saranu9@gmail.com>
2026-06-19 12:21:57 -0400

    Remove orphan pipeboard components importing missing shadcn/ui (broke Vercel build)

--- frontend/components/integrations/pipeboard/index.ts
+++
@@ -1,5 +0,0 @@
-export { MarketingSpendChart } from './MarketingSpendChart';
-export { CampaignTable } from './CampaignTable';
-export { ConnectionStatus } from './ConnectionStatus';
-export { CategoryMappingEditor } from './CategoryMappingEditor';
-export { AlertsPanel } from './AlertsPanel';
